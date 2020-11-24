e935fe102ae5ae9a2885e35cafaef9eb974ad123
Aaron <aaron.ar3llano@gmail.com>
2020-11-24 14:37:35 -0800

    Properly close db resources when servlet shuts down

--- src/goji/webapp/Startup.java
+++ src/goji/webapp/Startup.java
@@ -3,9 +3,9 @@ package goji.webapp;
 import java.util.logging.Logger;
 import javax.servlet.*;
 import javax.servlet.http.*;
-
 import goji.common.AgentUtils;
 import goji.common.GojiLogManagement;
+import goji.data.SqlClient;
 
 /** Startup class for the Goji backend web app. Initialization of the project begins here.
  * 
@@ -14,16 +14,24 @@ import goji.common.GojiLogManagement;
  */
 @SuppressWarnings("serial")
 public class Startup extends HttpServlet {
+	private AgentUtils agentUtils;
 	
 	private static final Logger LOGGER = GojiLogManagement.createLogger(Startup.class.getName());
 
-
+	@Override
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
 		
 		LOGGER.info("Setting up the agent environment");
-		AgentUtils agentUtils = new AgentUtils();
+		agentUtils = new AgentUtils();
 		agentUtils.getAgent().createAgentTables();
 		LOGGER.info("Done setting up the agent environment");
+	}
+	
+	@Override
+	public void destroy() {
+		SqlClient client = agentUtils.getAgent().getSqlClient();
+		LOGGER.info("Closing database connection as servlet is shutting down...");
+		client.closeConnection();
 	}
 }
